404085c4e6090a3bee237a7df663de11c3391b2a
Sebastian Portesi <sportesi@octopus.com.ar>
2019-09-06 11:45:58 -0300

    User Seeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Sebastian Portesi',
+            'email' => 'seba@seba.com',
+            'password' => bcrypt('123456'),
+        ]);
+    }
+}
