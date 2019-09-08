f037628ddc4973cfee4240b33065e160d7ab08a7
Sebastian Portesi <sportesi@octopus.com.ar>
2019-09-08 17:29:34 -0300

    Post order in homepage

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $posts = Post::orderBy('created_at')->paginate(5);
+        $posts = Post::orderBy('created_at', 'desc')->paginate(5);
 
         return view('home', [
             'posts' => $posts,
